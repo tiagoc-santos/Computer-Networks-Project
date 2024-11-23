eca5b6bb0abd3ee4f79894572fff4f83b59bce7d
Tiago Santos <tiagocsantos07@gmail.com>
2024-11-23 19:01:29 +0000

    input reading and handling done.

--- src/player/player.c
+++ src/player/player.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include "protocol.h"
+#include <ctype.h>
 
 #define CMD_SIZE 50
 #define ARG_SIZE 10
@@ -10,6 +11,8 @@
 int read_line(char *line){
     char c;
     int i = 0;
+
+    memset(line, 0, CMD_SIZE);
     while((c = getchar()) != '\n')
         line[i++] = c;
     line[i] = '\0';
@@ -42,11 +45,42 @@ int num_args(char arguments[ARG_SIZE][CMD_SIZE]){
     return res;
 }
 
+int check_digits(char* num_str){
+    int length = strlen(num_str);
+    int i;
+    for (i = 0; i < length; i++){
+        if (!isdigit(num_str[i]))
+            return 0;
+    }
+    return 1;
+}
+
+int validate_start(char cmd_args[ARG_SIZE][CMD_SIZE]){
+    return (num_args(cmd_args) != 3 || strlen(cmd_args[1]) != 6 || !check_digits(cmd_args[1])
+                || !check_digits(cmd_args[2]) || atoi(cmd_args[2]) > 600 || atoi(cmd_args[2]) < 1);
+}
+
+int is_valid_color(char* color){
+    return (!strcmp(color, "R") || !strcmp(color, "G") || !strcmp(color, "B") || !strcmp(color, "Y") ||
+                !strcmp(color, "O") || !strcmp(color, "P"));
+}
+
+int validate_try(char cmd_args[ARG_SIZE][CMD_SIZE]){
+    int i;
+
+    for (i = 1; i < 5; i++){
+        if (!is_valid_color(cmd_args[i]))
+            return 0;
+    }
+    return 1;
+}
+
 int main(int argc, char** argv){
     char* server_IP = SERVER_IP_DEFAULT;
     char* server_port = SERVER_PORT_DEFAULT;
     struct addrinfo *server_info;
     int player_udp_socket;
+    int game_running = 0;
 
     if(argc == 3){
         if (!strcmp(argv[1], "-n")){
@@ -72,7 +106,7 @@ int main(int argc, char** argv){
         }
 
     if (get_server_info(&server_info, server_IP, server_port, 1) != 0){
-        fprintf(stdout, "Error getting server info.\n");
+        fprintf(stderr, "Error getting server info.\n");
         exit(1);
     }
 
@@ -94,8 +128,62 @@ int main(int argc, char** argv){
 
         // start game command
         if (!strcmp(cmd_args[0], "start")){
-            if(num_args(cmd_args) != 3 ){}
+            if (!validate_start(cmd_args))
+                fprintf(stderr, "Invalid command.\n");
+
+            //funcao start
+            else{
+            }
+        }
+
+        // start game command
+        else if (!strcmp(cmd_args[0], "start")){
+            if (!validate_start(cmd_args))
+                fprintf(stderr, "Invalid command.\n");
+
+            //funcao start
+            else{
+                game_running = 1;
+            }
+        }
+
+        // try command
+        else if (!strcmp(cmd_args[0], "try")){
+            if (!validate_try(cmd_args))
+                fprintf(stderr, "Invalid command.\n");
+
+            //funcao try
+            else{
+            }
+        }
+
+        // show trials command
+        else if (!strcmp(cmd_args[0], "st") || !strcmp(cmd_args[0], "show_trials")){
+
+        }
+
+        // scoreboard command
+        else if (!strcmp(cmd_args[0], "sb") || !strcmp(cmd_args[0], "scoreboard")){
+            
+        }
+
+        // quit command
+        else if (!strcmp(cmd_args[0], "quit")){
+            if (game_running)
+                //manda msg  e meter game running a 0  
+                continue; 
+        }
+
+        // exit command
+        else if (!strcmp(cmd_args[0], "exit")){
+            
+        }
+
+        // debug command
+        else if (!strcmp(cmd_args[0], "debug")){
+            
         }
     }
+
     return 0;
 }
